99d892bed2cbf7f39513077b408e1279ba810d49
incidrthreat <incidrthreat@gmail.com>
2019-09-15 22:36:47 -0500

    added createssh uri

--- internal/webapp/routes.go
+++ internal/webapp/routes.go
@@ -11,8 +11,10 @@ func (app *App) Routes() *mux.Router {
 	r := mux.NewRouter() //.StrictSlash(true)
 
 	r.HandleFunc("/", app.Index).Methods("GET", "POST")
-	r.HandleFunc("/createtunnel", app.NewTunnel).Methods("GET", "POST")
+	r.HandleFunc("/tunnel/ssh", app.GetSSHTunnel).Methods("GET")
+	r.HandleFunc("/tunnel/createssh", app.CreateSSHTunnel).Methods("POST")
 
+	// static folder linking
 	fs := http.FileServer(http.Dir(app.StaticDir))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	r.PathPrefix("/").HandlerFunc(app.DefaultHandler)
