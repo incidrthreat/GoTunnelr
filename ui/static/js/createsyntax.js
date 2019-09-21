c1c65d72009d3801af09a6381a5a0083733b7362
incidrthreat <incidrthreat@gmail.com>
2019-09-21 00:18:39 -0500

    added more syntax and additional options

--- ui/static/js/createsyntax.js
+++ ui/static/js/createsyntax.js
@@ -3,9 +3,12 @@ function myonload() {
 
 function getInputs() {
 
-    var user = document.querySelector('input[name="user"]');
-    var orgIP = document.querySelector('input[name="orgIP"]');
-    var destIP = document.querySelector('input[name="destIP"]');
+    var sshuser = document.querySelector('input[name="sshuser"]');
+    var sshserverip = document.querySelector('input[name="sshserverip"]');
+    var sshserverport = document.querySelector('input[name="sshserverport"]');
+    var remotehostip = document.querySelector('input[name="remotehostip"]');
+    var localport = document.querySelector('input[name="localport');
+    var remotehostport = document.querySelector('input[name="remotehostport');
     var radios = document.querySelectorAll('[name="tuntype"]');
     var resultsTag = document.getElementById("ssh-results");
 
@@ -15,12 +18,27 @@ function getInputs() {
         }
     }
 
+    // dynamic tunnel syntax
     if (tuntype == "dynamic") {
-        var resultsString = `ssh -p 22 -NfD ${user.value}@${destIP.value}`;
+        if (sshserverport.value == "") {
+            var resultsString = `ssh -p 22 -NfD ${localport.value} ${sshuser.value}@${sshserverip.value}`;
+        } else {
+            var resultsString = `ssh -p ${sshserverport.value} -NfD ${localport.value} ${sshuser.value}@${sshserverip.value}`;
+        }
+    // remote tunnel syntax
     } else if (tuntype == "remote") {
-        var resultsString = `ssh -p 22 -NfR 9000:localhost:9000 ${user.value}@${destIP.value}`
+        if (sshserverport.value == "") {
+            var resultsString = `ssh -p 22 -NfR ${localport.value}:${remotehostip.value}:${remotehostport.value} ${sshuser.value}@${sshserverip.value}`
+        } else {
+            var resultsString = `ssh -p ${sshserverport.value} -NfR ${localport.value}:${remotehostip.value}:${remotehostport.value} ${sshuser.value}@${sshserverip.value}`
+        }
+    // local tunnel syntax
     } else if (tuntype == "local") {
-        var resultsString = `ssh -p 22 -NfL 9000:localhost:9000 ${user.value}@${destIP.value}`
+        if (sshserverport.value == "") {
+            var resultsString = `ssh -p 22 -NfL ${localport.value}:${remotehostip.value}:${remotehostport.value} ${sshuser.value}@${sshserverip.value}`
+        } else {
+            var resultsString = `ssh -p ${sshserverport.value} -NfL ${localport.value}:${remotehostip.value}:${remotehostport.value} ${sshuser.value}@${sshserverip.value}`
+        }
     }
     var preArea = document.createElement("pre");
     preArea.innerHTML = resultsString;
