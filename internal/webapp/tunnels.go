e7510be954f32e6a550a05ccdefdec5e4d661acb
incidrthreat <incidrthreat@gmail.com>
2019-09-21 19:03:13 -0500

    added new GetNetshTunnel func

--- internal/webapp/tunnels.go
+++ internal/webapp/tunnels.go
@@ -8,3 +8,8 @@ import (
 func (app *App) GetSSHTunnel(w http.ResponseWriter, r *http.Request) {
 	app.RenderHTML(w, r, "ssh.html", nil)
 }
+
+// GetNetshTunnel ...
+func (app *App) GetNetshTunnel(w http.ResponseWriter, r *http.Request) {
+	app.RenderHTML(w, r, "netsh.html", nil)
+}
