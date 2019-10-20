daa1c6b7da5db7ee07232df458e336b8688eb063
epi052 <epibar052@gmail.com>
2019-10-19 21:15:54 -0500

    added dropdown menus, fixed footer, inserted content, added routing for some but not all linked content

--- internal/webapp/routes.go
+++ internal/webapp/routes.go
@@ -13,6 +13,9 @@ func (app *App) Routes() *mux.Router {
 	r.HandleFunc("/", app.Index).Methods("GET")
 	r.HandleFunc("/tunnel/ssh", app.GetSSHTunnel).Methods("GET")
 	r.HandleFunc("/tunnel/netsh", app.GetNetshTunnel).Methods("GET")
+	r.HandleFunc("/tunnel/sockets", app.GetSSHSockets).Methods("GET")
+	r.HandleFunc("/tunnel/networking", app.GetSSHNetworking).Methods("GET")
+	
 
 	// static folder linking
 	fs := http.FileServer(http.Dir(app.StaticDir))
--- internal/webapp/tunnels.go
+++ internal/webapp/tunnels.go
@@ -13,3 +13,11 @@ func (app *App) GetSSHTunnel(w http.ResponseWriter, r *http.Request) {
 func (app *App) GetNetshTunnel(w http.ResponseWriter, r *http.Request) {
 	app.RenderHTML(w, r, "netsh.html", nil)
 }
+
+func (app *App) GetSSHSockets(w http.ResponseWriter, r *http.Request) {
+	app.RenderHTML(w, r, "sockets.html", nil)
+}
+
+func (app *App) GetSSHNetworking(w http.ResponseWriter, r *http.Request) {
+	app.RenderHTML(w, r, "networking.html", nil)
+}
